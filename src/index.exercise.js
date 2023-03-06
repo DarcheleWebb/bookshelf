d6ca7ae7a80d817c01fb2d8351c2543e89f130b1
Darchele Webb <Darchele.Webb@rgare.com>
2023-03-06 07:41:08 -0600

    extra #01

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,19 +1,38 @@
 import * as React from 'react'
 import {createRoot} from 'react-dom/client'
 import {Logo} from './components/logo'
+import {Dialog} from '@reach/dialog'
+import '@reach/dialog/styles.css'
 
 function App() {
-  return (
+    const [showLoginDiag, setShowLoginDiag] = React.useState(false);
+    const [showRegisterDiag, setShowRegisterDiag] = React.useState(false);
+
+    const closeLogin = () => setShowLoginDiag(false)
+    const closeRegister = () => setShowRegisterDiag(false)
+
+    return (
     <div>
       <Logo width="80" height="80" />
       <h1>Bookshelf</h1>
       <div>
-        <button onClick={() => alert('login clicked')}>Login</button>
+        <button onClick={() => { setShowLoginDiag(true) }}>Login</button>
       </div>
       <div>
-        <button onClick={() => alert('register clicked')}>Register</button>
+        <button onClick={() => { setShowRegisterDiag(true) }}>Register</button>
       </div>
+
+      <Dialog isOpen={showLoginDiag} onDismiss={closeLogin}>
+        <button onClick={() => closeLogin()}>Close</button>
+        <h2>Login</h2>
+      </Dialog>
+
+      <Dialog isOpen={showRegisterDiag} onDismiss={closeRegister}>
+        <button onClick={() => closeRegister()}>Close</button>
+        <h2>Register</h2>
+      </Dialog>
     </div>
+
   )
 }
 
